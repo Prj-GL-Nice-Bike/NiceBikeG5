d43b446e30dbdb9adf7a97d7fdc74d4f1903e578
Oussama Sahib <195052@ecam.be>
2023-03-04 15:03:12 +0100

    SR (Page Login)

---
+++ NiceBikeG5/SR_Menu.xaml.cs
@@ -0,0 +1,9 @@
+namespace NiceBikeG5;
+
+public partial class SR_Menu: ContentPage
+{
+	public SR_Menu()
+	{
+		InitializeComponent();
+	}
+}
--- NiceBikeG5/Session_SR.xaml.cs
+++ NiceBikeG5/Session_SR.xaml.cs
@@ -1,9 +1,27 @@
 ﻿namespace NiceBikeG5;
 
-public partial class Session_SR : ContentPage
+public partial class Session_SR: ContentPage
 {
 	public Session_SR()
 	{
 		InitializeComponent();
 	}
+
+    /*FCT BUTTON BACK*/
+    private async void OnButton_Back(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
+
+    /*FCT BUTTON LOGIN*/
+    private void OnButton_LogIn(object sender, EventArgs e)
+    {
+        string password= PasswordEntry.Text;
+        //If Password: Next Page
+        if (password=="123")
+        {Navigation.PushAsync(new SR_Menu());}
+        //If not Password: Error Message
+        else
+        {DisplayAlert("ERROR", "Incorrect password", "OK");}
+    }
 }
