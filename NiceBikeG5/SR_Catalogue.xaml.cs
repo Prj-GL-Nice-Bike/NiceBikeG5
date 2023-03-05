e77fba0fdc27221b17b26d43a6987178c6df0ca2
Oussama Sahib <195052@ecam.be>
2023-03-05 23:19:20 +0100

    SR (Catalogue -HomePage)

--- NiceBikeG5/SR_Catalogue.xaml.cs
+++ NiceBikeG5/SR_Catalogue.xaml.cs
@@ -1,9 +1,35 @@
 namespace NiceBikeG5;
 
-public partial class SR_Catalogue : ContentPage
+public partial class SR_Catalogue: ContentPage
 {
 	public SR_Catalogue()
 	{
 		InitializeComponent();
 	}
+
+    /*FCT BUTTON BACK*/
+    private async void OnButton_Back(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SR_Menu());
+    }
+
+    /*FCT BUTTON CART*/
+    private async void OnButton_CartCatalogue(object sender, EventArgs e)
+    {
+    }
+
+    /*FCT IMAGEBUTTON BIKE N°1*/
+    private async void OnButton_Bike1(object sender, EventArgs e)
+    {
+    }
+
+    /*FCT IMAGEBUTTON BIKE N°2*/
+    private async void OnButton_Bike2(object sender, EventArgs e)
+    {
+    }
+
+    /*FCT IMAGEBUTTON BIKE N°3*/
+    private async void OnButton_Bike3(object sender, EventArgs e)
+    {
+    }
 }
