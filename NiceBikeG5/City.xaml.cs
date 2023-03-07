8c516a5daf76f60c84bd357d15ddecb3dd061aee
StephLvs <195379@ecam.be>
2023-03-07 16:53:13 +0100

    Bike pages

---
+++ NiceBikeG5/City.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace NiceBikeG5;
+
+public partial class City : ContentPage
+{
+	public City()
+	{
+		InitializeComponent();
+	}
+    private async void OnButton_Back(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SR_Catalogue());
+    }
+    private void AddToCart(object sender, EventArgs e)
+    {
+    }
+    private async void GoToCart(object sender, EventArgs e)
+    {
+        //await Navigation.PushAsync(new Cart());
+    }
+}
