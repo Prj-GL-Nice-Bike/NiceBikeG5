479e55da100323aa93b2cc71b67ebafd2c25ac2a
Oussama Sahib <195052@ecam.be>
2023-03-07 17:57:12 +0100

    SR (Delete Title of pages)

--- NiceBikeG5/SR_Catalogue.xaml.cs
+++ NiceBikeG5/SR_Catalogue.xaml.cs
@@ -22,15 +22,18 @@ public partial class SR_Catalogue: ContentPage
     /*FCT IMAGEBUTTON BIKE N°1*/
     private async void OnButton_Bike1(object sender, EventArgs e)
     {
+        await Navigation.PushAsync(new City());
     }
 
     /*FCT IMAGEBUTTON BIKE N°2*/
     private async void OnButton_Bike2(object sender, EventArgs e)
     {
+        await Navigation.PushAsync(new Explorer());
     }
 
     /*FCT IMAGEBUTTON BIKE N°3*/
     private async void OnButton_Bike3(object sender, EventArgs e)
     {
+        await Navigation.PushAsync(new Adventure());
     }
 }
