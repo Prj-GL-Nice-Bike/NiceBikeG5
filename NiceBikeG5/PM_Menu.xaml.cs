a1c48ad6ab2675f44dfc2125f937b979eed47870
Oussama Sahib <195052@ecam.be>
2023-03-10 14:22:37 +0100

    PM (Page Login)

---
+++ NiceBikeG5/PM_Menu.xaml.cs
@@ -0,0 +1,9 @@
+namespace NiceBikeG5;
+
+public partial class PM_Menu : ContentPage
+{
+	public PM_Menu()
+	{
+		InitializeComponent();
+	}
+}
--- NiceBikeG5/Session_PM.xaml.cs
+++ NiceBikeG5/Session_PM.xaml.cs
@@ -2,8 +2,42 @@
 
 public partial class Session_PM : ContentPage
 {
-	public Session_PM()
+    string password;
+
+    public Session_PM()
 	{
 		InitializeComponent();
 	}
+
+	/*FCT BUTTON BACK*/
+    private async void OnButton_Back(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
+
+
+    /*FCT BUTTON LOGIN*/
+    //CLICK ON THE BUTTON
+    private void OnButton_LogIn(object sender, EventArgs e)
+    {
+        password= PasswordEntry.Text;
+        //If Password: Next Page
+        if(password=="abc")
+        {Navigation.PushAsync(new PM_Menu());}
+        //If not Password: Error Message
+        else
+        {DisplayAlert("ERROR", "Incorrect password", "OK");}
+    }
+
+    //ENTER
+    async void OnEntryCompleted(object sender, EventArgs e)
+    {
+        password= ((Entry)sender).Text;
+        //If Password: Next Page
+        if(password=="abc")
+        {await Navigation.PushAsync(new PM_Menu());}
+        //If not Password: Error Message
+        else
+        {await DisplayAlert("ERROR", "Incorrect password", "OK");}
+    }   
 }
