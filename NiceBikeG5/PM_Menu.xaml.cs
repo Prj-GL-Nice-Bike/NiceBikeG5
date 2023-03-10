bac4c5b5b3b8fa0ab3967312b24303eab1c40365
Oussama Sahib <195052@ecam.be>
2023-03-10 15:05:38 +0100

    PM (Menu)

--- NiceBikeG5/PM_Menu.xaml.cs
+++ NiceBikeG5/PM_Menu.xaml.cs
@@ -6,4 +6,22 @@ public partial class PM_Menu : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    /*FCT BUTTON LOGOUT*/
+    private async void OnButton_LogOut(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Session_PM());
+    }
+
+    /*FCT BUTTON LIST OF ORDERS*/
+    private async void OnButton_ListOrders(object sender, EventArgs e)
+    {
+        
+    }
+
+    /*FCT BUTTON INVENTORY*/
+    private async void OnButton_Inventory(object sender, EventArgs e)
+    {
+        
+    }
 }
