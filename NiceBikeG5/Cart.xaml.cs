bbf17fd841322d7b8e0c49932ff0551a5e67e33c
StephLvs <195379@ecam.be>
2023-03-13 22:33:07 +0100

    Updated bike and cart pages with link to MySQL database

--- NiceBikeG5/Cart.xaml.cs
+++ NiceBikeG5/Cart.xaml.cs
@@ -1,23 +1,101 @@
 using Microsoft.Maui.Controls;
-using NiceBikeG5;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using System;
 using System.Collections.ObjectModel;
 
-using static System.Net.WebRequestMethods;
 
-namespace NiceBikeG5;
-public partial class Cart : ContentPage
+namespace NiceBikeG5
 {
-    public Cart()
-	{
-        InitializeComponent();
-    }
-    
-    private async void ReturnPrevious(object sender, EventArgs e)
+    public partial class Cart : ContentPage
     {
-        await Shell.Current.GoToAsync(nameof(SR_Catalogue)); 
+        private ObservableCollection<Order> _orders;
+
+        public Cart()
+        {
+            InitializeComponent();
+            BindingContext = new CartViewModel();
+
+            //REFRESHES THE PAGE 
+            _orders = ((CartViewModel)BindingContext).Orders;
+        }
+
+        // DELETE BUTTON
+        private async void DeleteOrder(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            // BINDS THE PARAMETERS OF THE CLASS ORDER
+            var order = (Order)button.BindingContext;
+
+            // CONNECTION WITH MYSQL
+            var connectionString = "Server=localhost;Database=bikes;Uid=root;Pwd=root;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            var commandText = $"DELETE FROM orders WHERE idorders = {order.Id};";
+            using var command = new MySqlCommand(commandText, connection);
+            await command.ExecuteNonQueryAsync();
+
+            _orders.Remove(order);
+        }
+        // NAVIGATION BUTTONS
+        private async void ReturnPrevious(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(SR_Catalogue));
+        }
+
+        private async void GotoClients(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(SRSellers));
+        }
+
     }
-    private async void GotoClients(object sender, EventArgs e)
+
+    // VIEW MODEL
+    public class CartViewModel
     {
-        await Shell.Current.GoToAsync(nameof(SRSellers));
+        public ObservableCollection<Order> Orders { get; }
+
+        public CartViewModel()
+        {
+            Orders = new ObservableCollection<Order>();
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            // CONNECTION WITH MYSQL
+            var connectionString = "Server=localhost;Database=bikes;Uid=root;Pwd=root;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            var commandText = "SELECT * FROM orders;";
+            using var command = new MySqlCommand(commandText, connection);
+            using var reader = command.ExecuteReader();
+
+            // READS LINE BY LINE IN MYSQL DATABASE
+            while (await reader.ReadAsync())
+            {
+                var id = reader.GetInt32("idorders");
+                var productName = reader.GetString("Type");
+                var productSize = reader.GetDouble("Size");
+                var productColor = reader.GetString("Color");
+                var productQuantity = reader.GetDouble("Quantity");
+                Orders.Add(new Order { Id = id, ProductName = productName, ProductSize = productSize, ProductColor = productColor, ProductQuantity = productQuantity });
+            }
+        }
     }
+
+    // PARAMETERS
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public double ProductSize { get; set; }
+        public string ProductColor { get; set; }
+        public double ProductQuantity { get; set; }
+    }
+
 }
+
--- NiceBikeG5/Explorer.xaml.cs
+++ NiceBikeG5/Explorer.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.Maui.Controls;
+using MySql.Data.MySqlClient;
+
 namespace NiceBikeG5;
 
 public partial class Explorer : ContentPage
 {
-    string color = "";
-
 
     public Explorer()
     {
@@ -14,24 +14,41 @@ public partial class Explorer : ContentPage
         sizePicker.SelectedItem = 26;
         quantityPicker.SelectedItem = 1;
         colorPicker.SelectedItem = "Red";
+
     }
 
-    //BUTTONS
+    // NAVIGATION BUTTONS
     private async void OnButton_Back(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new SR_Catalogue());
-    }
-    private async void AddToCart(object sender, EventArgs e)
-    {
-        color = colorPicker.SelectedItem.ToString();
-        await DisplayAlert("Added to cart", "", "OK");
-
     }
     private async void GoToCart(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Cart());
     }
 
+    private async void AddToCart(object sender, EventArgs e)
+    {
+        // TYPE, SIZE, COLOR AND QUANTITY FROM THE PICKERS
+        string productType = "Explorer";
+        double productSize = double.Parse(sizePicker.SelectedItem.ToString());
+        string productColor = colorPicker.SelectedItem.ToString();
+        double productQuantity = double.Parse(quantityPicker.SelectedItem.ToString());
+
+        // CONNECTION WITH MYSQL
+        var connectionString = "Server=localhost;Database=bikes;Uid=root;Pwd=root;";
+        using var connection = new MySqlConnection(connectionString);
+        connection.Open();
+        // INSERT NEW BIKE TO THE ORDERS TABLE
+        var commandText = $"INSERT INTO orders (Type, Size, Color, Quantity) VALUES ('{productType}', '{productSize}', '{productColor}', '{productQuantity}')";
+        using var command = new MySqlCommand(commandText, connection);
+        command.ExecuteNonQuery();
+
+        await DisplayAlert("Added to cart", "", "OK");
+
+    }
+
+    // TOTAL CALCULATION
     private void TotalPrice(object sender, EventArgs e)
     {
         if (int.TryParse(quantityPicker.SelectedItem.ToString(), out int selectedQuantityValue) && int.TryParse(sizePicker.SelectedItem.ToString(), out int selectedSizeValue))
