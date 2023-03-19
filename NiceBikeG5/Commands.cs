550b97c51145b8cc2256cfc716b5252d08281d4c
radmou99 <mouradm.mm@outlook.com>
2023-03-19 02:19:53 +0100

    NBP-87

---
+++ NiceBikeG5/Commands.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Threading.Tasks;
+
+using Mysqlx.Prepare;
+
+
+namespace NiceBikeG5
+{
+
+    public class Commands : INotifyPropertyChanged
+    {
+        public ICommand Bikecommand { private set; get; }
+        public ICommand Componentcommand { private set; get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Commands()
+        {
+            Bikecommand = new Command(
+                execute: () =>
+                { },
+                canExecute: () =>
+                {
+                    return false;
+                });
+
+            Componentcommand = new Command(
+                execute: () =>
+                {  },
+                canExecute: () =>
+                {
+                    return false;
+                });
+        }
+        void RefreshCanExecutes()
+        {
+            (Componentcommand as Command).ChangeCanExecute();
+            (Bikecommand as Command).ChangeCanExecute();
+        }
+
+    }
+}
+   
+
